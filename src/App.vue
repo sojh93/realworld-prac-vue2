3823077a711a5bde856ee618075ef1d0a2d30eda
jhso93 <jhso@m-ultiply.com>
2023-06-13 13:06:06 +0900

    vue-router 생성 (vuerouter 3.x는 vue2.x랑만 호환)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <header-component></header-component>
+    <router-view></router-view>
     <footer-component></footer-component>
   </div>
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -82,7 +82,7 @@
 <script>
 
 export default {
-  name: 'HomeMain',
+  name: 'HomePage',
   components: {
   }
 }
